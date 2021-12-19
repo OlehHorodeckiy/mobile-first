b89a4370405850e70779afc26f91c8d5a7e306f1
олег <gorodeckiy88@gmail.com>
2021-12-19 10:32:38 +0200

    added new tabs

--- IRC/js/main.js
+++ IRC/js/main.js
@@ -4,3 +4,20 @@ const menu =document.querySelector('.mobile-menu')
 button.addEventListener('click', event => {
     menu.classList.toggle('active');
 });
+//tabs
+const jsTriggers = document.querySelectorAll('.js-tab-trigger'),
+    jsContents = document.querySelectorAll('.js-tab-content');
+jsTriggers.forEach(function(trigger) {
+    trigger.addEventListener('click', function() {
+        const id = this.getAttribute('data-tab'),
+            content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+            activeTrigger = document.querySelector('.js-tab-trigger.active'),
+            activeContent = document.querySelector('.js-tab-content.active');
+
+        activeTrigger.classList.remove('active'); // 1
+        trigger.classList.add('active'); // 2
+
+        activeContent.classList.remove('active'); // 3
+        content.classList.add('active'); // 4
+    });
+});
